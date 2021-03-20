cdc6fc9d74b1dfb42f1eb16f2a0cd646fde2079a
NorbertSzczepanski <74811587+NorbertSzczepanski@users.noreply.github.com>
2021-03-20 18:41:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ listasztuki = [x for x, wartosc in produkty.items() if wartosc == 'szt']
 print("Produkty kupowane na sztuki to: " + str(listasztuki))
 
 print('\nZadanie4')
+
+
 def trojkat_prostokatny(a, b, c):
     if (a + b > c) & (b + c > a) & (a + c > b):
         if (a ** 2 + b ** 2 == c ** 2) or (c ** 2 + b ** 2 == a ** 2) or (a ** 2 + c ** 2 == b ** 2):
@@ -34,18 +36,31 @@ def trojkat_prostokatny(a, b, c):
     else:
         print('To nie jest trójkąt.')
 
+
 print('Podaj boki trójkąta, sprawdzę czy jest prostokątny.')
 x = float(input('Podaj długość pierwszego boku: '))
 y = float(input('Podaj długość drugiego boku: '))
 z = float(input('Podaj długość trzeciego boku: '))
-trojkat_prostokatny(x,y,z)
+trojkat_prostokatny(x, y, z)
+
 print('\nZadanie5')
 
 
-def poletrapezu(a=4, b=5, h=6):
-    trapez = [a, b, h]
+def pole_trapezu(a=2, b=7, h=4):
     pole = ((a + b) * h) / 2
     return pole
 
 
-print('Pole trapezu: ' + str(poletrapezu()))
+print('Pole trapezu wynosi: ' + str(pole_trapezu()))
+
+print('\nZadanie6')
+
+
+def iloczyn_ciagow(a=1, b=4, ile=10):
+    wynik = a
+    for x in range(ile):
+        wynik = wynik * (a + b * x)
+    return wynik
+
+
+print(iloczyn_ciagow(ile=4))
